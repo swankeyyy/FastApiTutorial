51342399db1399e89067dc58cc11f7feb6f8c6a6
levchuk <swankyyy1@gmail.com>
2024-09-10 11:10:39 +0300

    no more create tables on app start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from contextlib import asynccontextmanager
 from core.config import settings
-from core.models import Base, db_helper
 from fastapi import FastAPI
 import uvicorn
 from items import router as items_router
@@ -8,11 +7,10 @@ from users.views import router as users_router
 from api_v1 import router as router_v1
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#
+#     yield
 
 
 app = FastAPI(lifespan=lifespan)
